73358cb87b2b0070f6de81a8c30628c821af1236
Alexandre Dubois <contact@alexandredubois.com>
2015-05-06 21:34:18 +0000

    Getproduct implementation started

---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Request/GetProductRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Cdiscount.OpenApi.ProxyClient.Contract.Data;
+using Newtonsoft.Json;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Request
+{
+    /// <summary>
+    /// Request object to retrieve product(s)
+    /// </summary>
+    public class GetProductRequest
+    {
+        /// <summary>
+        /// Product Identifier list
+        /// </summary>
+        public List<string> ProductIdList { get; set; }
+
+        /// <summary>
+        /// Product EAN  list
+        /// </summary>
+        [JsonProperty(PropertyName = "EANList")]
+        public List<string> EanList { get; set; }
+
+        /// <summary>
+        /// Additional product information to retrieve
+        /// </summary>
+        public GetProductRequestScope Scope { get; set; }
+
+
+    }
+}
