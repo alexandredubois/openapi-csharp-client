7a27c00eb050f3b96ed08526409b6697dc0b1dd5
Adrien Havas <adrienhavas@gmail.com>
2015-05-11 22:09:08 +0200

    GetCart correction for only one item

---
+++ Cdiscount.OpenApi.ProxyClient/Helper/SingleValueArrayConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Cdiscount.OpenApi.ProxyClient.Helper
+{
+    /// <summary>
+    /// Parse Json into a List of objects, whether the square brackets [] are there or not 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SingleValueArrayConverter<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object result = null;
+
+            if (reader.TokenType == JsonToken.StartObject)
+            {
+                T instance = (T)serializer.Deserialize(reader, typeof(T));
+                result = new List<T>() { instance };
+            }
+            else if (reader.TokenType == JsonToken.StartArray)
+            {
+                result = serializer.Deserialize(reader, objectType);
+            }
+
+            return result;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
