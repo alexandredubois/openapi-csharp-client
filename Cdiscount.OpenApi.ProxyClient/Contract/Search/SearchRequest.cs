e278dd70d570e96ab8d4c929ffc86cf39693892a
Alexandre Dubois <contact@alexandredubois.com>
2015-05-11 22:55:43 +0200

    Search method ready to be implemented

---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Search/SearchRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Cdiscount.OpenApi.ProxyClient.Contract.Search
+{
+    public class SearchRequest
+    {
+        public string Keyword { get; set; }
+    }
+}
---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Search/SearchRequestWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace Cdiscount.OpenApi.ProxyClient.Contract.Search
+{
+    public class SearchRequestWrapper
+    {
+        /// <summary>
+        /// Private cdiscount open api key
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        public SearchRequest SearchRequest { get; set; }
+    }
+}
