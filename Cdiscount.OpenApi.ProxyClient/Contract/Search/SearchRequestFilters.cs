6fd15cfbcfb572ff9aed7d8554d7ebd71c376616
Drenskin <drenskin@gmail.com>
2015-05-12 20:39:54 +0000

    Search method implementation started. Forgotten merges

---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Search/SearchRequestFilters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Cdiscount.OpenApi.ProxyClient.Helper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Search
+{
+    public class SearchRequestFilters
+    {
+        public SearchRequestFiltersPrice Price { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public SearchRequestFiltersNavigation Navigation { get; set; }
+
+        public bool IncludeMarketPlace { get; set; }
+
+        //[JsonConverter(typeof(ToLowerCaseStringEnumerableConverter))]
+        //public List<string> Brands { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public SearchRequestFiltersCondition Condition { get; set; }
+    }
+}
---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Search/SearchRequestFiltersNavigation.cs
@@ -0,0 +1,97 @@
+﻿using System.Runtime.Serialization;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Search
+{
+    public enum SearchRequestFiltersNavigation
+    {
+        [EnumMember(Value = "all")]
+        All,
+
+        [EnumMember(Value = "pets")]
+        Pets,
+
+        [EnumMember(Value = "kitchen")]
+        Kitchen,
+
+        [EnumMember(Value = "vehicles")]
+        Vehicles,
+
+        [EnumMember(Value = "baggage")]
+        Baggage,
+
+        [EnumMember(Value = "luxury")]
+        Luxury,
+
+        [EnumMember(Value = "tools")]
+        Tools,
+
+        [EnumMember(Value = "shoes")]
+        Shoes,
+
+        [EnumMember(Value = "packaging")]
+        Packaging,
+
+        [EnumMember(Value = "entertainment")]
+        Entertainment,
+
+        [EnumMember(Value = "home")]
+        Home,
+
+        [EnumMember(Value = "hardware")]
+        Hardware,
+
+        [EnumMember(Value = "appliances")]
+        Appliances,
+
+        [EnumMember(Value = "electronics")]
+        Electronics,
+
+        [EnumMember(Value = "grocery")]
+        Grocery,
+
+        [EnumMember(Value = "cosmetics")]
+        Cosmetics,
+
+        [EnumMember(Value = "computers")]
+        Computers,
+
+        [EnumMember(Value = "musical instruments")]
+        MusicalInstruments,
+
+        [EnumMember(Value = "garden")]
+        Garden,
+
+        [EnumMember(Value = "toys")]
+        Toys,
+
+        [EnumMember(Value = "books")]
+        Books,
+
+        [EnumMember(Value = "haberdashery")]
+        Haberdashery,
+
+        [EnumMember(Value = "furniture")]
+        Furniture,
+
+        [EnumMember(Value = "cameras")]
+        Cameras,
+
+        [EnumMember(Value = "childcare")]
+        Childcare,
+
+        [EnumMember(Value = "sport")]
+        Sport,
+
+        [EnumMember(Value = "phones")]
+        Phones,
+
+        [EnumMember(Value = "tv-audio")]
+        TvAudio,
+
+        [EnumMember(Value = "clothing")]
+        Clothing,
+
+        [EnumMember(Value = "wines-spirits")]
+        WinesSpirits
+    }
+}
