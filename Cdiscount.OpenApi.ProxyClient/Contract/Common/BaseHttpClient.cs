e13ac33d6445ecd56aabe59a9ca655ea7504bd3c
Drenskin <drenskin@gmail.com>
2015-05-12 20:31:07 +0000

    Factorisation of OpenApiClient

---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Common/BaseHttpClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
+{
+    public class BaseHttpClient : HttpClient
+    {
+        private const string ApiAddress = "https://api.cdiscount.com/";
+
+        public BaseHttpClient()
+        {
+            this.BaseAddress = new Uri(ApiAddress);
+            this.DefaultRequestHeaders.Accept.Clear();
+            this.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Cdiscount.OpenApi.ProxyClient/Contract/Common/BaseHttpContent.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
+{
+    public class BaseHttpContent : StringContent
+    {
+        public BaseHttpContent(string content)
+            : base(content, Encoding.UTF8)
+        {
+            this.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+}
