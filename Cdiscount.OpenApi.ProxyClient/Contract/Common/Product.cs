227f622798de42135cf9005fcb50d00eb5f0fe2d
Drenskin <drenskin@gmail.com>
2015-05-13 18:15:18 +0200

    Product moved in Common & new Converter for Brands

--- Cdiscount.OpenApi.ProxyClient/Contract/Common/Product.cs
+++ Cdiscount.OpenApi.ProxyClient/Contract/Common/Product.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace Cdiscount.OpenApi.ProxyClient.Contract.GetProduct
+namespace Cdiscount.OpenApi.ProxyClient.Contract.Common
 {
     /// <summary>
     /// Product details
---
+++ Cdiscount.OpenApi.ProxyClient/Helper/ToLowerCaseStringEnumerableConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Cdiscount.OpenApi.ProxyClient.Helper
+{
+    /// <summary>
+    /// Convert the one item to a list while the API doesn't support more. Also convert to lowercases.
+    /// </summary>
+    public class ToLowerCaseStringEnumerableConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            object toSerialize;
+            string stringToSend = value as string;
+
+            if (stringToSend != null)
+            {
+                toSerialize = new List<string> { stringToSend.ToLowerInvariant() };
+            }
+            else
+            {
+                toSerialize = value;
+            }
+
+            serializer.Serialize(writer, toSerialize);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(IEnumerable<string>);
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
