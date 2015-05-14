3bff3c4d2c01cf3fa73626871f4743c8196813b3
Alexandre Dubois <contact@alexandredubois.com>
2015-05-14 12:03:23 +0200

    Nuget packaging

--- Cdiscount.OpenApi.ProxyClient/Properties/AssemblyInfo.cs
+++ Cdiscount.OpenApi.ProxyClient/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cdiscount.OpenApi.ProxyClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This open source project is here to help people wanting to use the Cdiscount Open API in a .NET powered solution. This project was designed as a thin C# wrapper on top of the Cdiscount Open API. Packaged as a portable class library, you can use it on different .NET environments (Windows Phone 8+ app, Windows Store App...).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cdiscount.OpenApi.ProxyClient")]
