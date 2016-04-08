5d2cc19ebae044cff1f8bb0f320eaa5deeedb2af
Adrien Havas <adrienhavas@gmail.com>
2016-04-09 00:28:19 +0200

    Tests added for Marketplace products and products with a size

--- Cdiscount.OpenApi.ProxyClient.Tests/PushToCartTests.cs
+++ Cdiscount.OpenApi.ProxyClient.Tests/PushToCartTests.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Cdiscount.OpenApi.ProxyClient.Contract.PushToCart;
 using Cdiscount.OpenApi.ProxyClient.Tests.Helper;
+using System.Collections.Generic;
+using Cdiscount.OpenApi.ProxyClient.Contract.GetProduct;
+using System.Linq;
 
 namespace Cdiscount.OpenApi.ProxyClient.Tests
 {
@@ -68,6 +71,80 @@ namespace Cdiscount.OpenApi.ProxyClient.Tests
             Assert.IsNotNull(response.CartGuid);
             Assert.IsNotNull(response.CheckoutUrl);
             Assert.AreEqual(cart.CartGuid, response.CartGuid);
+        }
+
+        [TestMethod]
+        public void PushToCart_AddItemMkp_OperationSuccess()
+        {
+            var getProduct = _openApiProxyClient.GetProduct(new GetProductRequest
+            {
+                ProductIdList = new List<string> { "nikd52001855vr2" },
+                Scope = new GetProductRequestScope
+                {
+                    Ean = true,
+                    Offers = true
+                }
+            });
+
+            Assert.IsTrue(getProduct.OperationSuccess);
+            Assert.IsTrue(getProduct.Products != null && getProduct.Products.Any());
+
+            var offer = getProduct.Products[0].Offers.First(o => o.Seller.Id > 0);
+
+            var request = new PushToCartRequest
+            {
+                ProductId = getProduct.Products[0].Id,
+                OfferId = offer.Id,
+                SellerId = offer.Seller.Id,
+                Quantity = 1,
+                SizeId = null
+            };
+
+            var response = _openApiProxyClient.PushToCart(request);
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.OperationSuccess);
+            Assert.AreEqual("NoError", response.ErrorType);
+            Assert.IsTrue(string.IsNullOrEmpty(response.ErrorMessage));
+            Assert.IsNotNull(response.CartGuid);
+            Assert.IsNotNull(response.CheckoutUrl);
+        }
+
+        [TestMethod]
+        public void PushToCart_AddItemMkpWithVariant_OperationSuccess()
+        {
+            var getProduct = _openApiProxyClient.GetProduct(new GetProductRequest
+            {
+                ProductIdList = new List<string> { "mp02814077" },
+                Scope = new GetProductRequestScope
+                {
+                    Ean = true,
+                    Offers = true
+                }
+            });
+
+            Assert.IsTrue(getProduct.OperationSuccess);
+            Assert.IsTrue(getProduct.Products != null && getProduct.Products.Any());
+
+            var offer = getProduct.Products[0].Offers.First(o => o.Seller.Id > 0);
+
+            var request = new PushToCartRequest
+            {
+                ProductId = getProduct.Products[0].Id,
+                OfferId = offer.Id,
+                SellerId = offer.Seller.Id,
+                Quantity = 1,
+                SizeId = offer.Sizes.First(s => s.IsAvailable).Id
+            };
+
+            var response = _openApiProxyClient.PushToCart(request);
+
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.OperationSuccess);
+            Assert.AreEqual("NoError", response.ErrorType);
+            Assert.IsTrue(string.IsNullOrEmpty(response.ErrorMessage));
+            Assert.IsNotNull(response.CartGuid);
+            Assert.IsNotNull(response.CheckoutUrl);
         }
     }
 }
